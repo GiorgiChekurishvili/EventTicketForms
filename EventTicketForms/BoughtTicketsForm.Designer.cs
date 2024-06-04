4774a1db920f698c86f1f34748d570df1ee4be33
Giorgi Chekurishvili <giorgichekurishvili04@gmail.com>
2024-06-05 01:11:45 +0400

    Bought tickets form added

---
+++ EventTicketForms/BoughtTicketsForm.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace EventTicketForms
+{
+    partial class BoughtTicketsForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel5 = new Panel();
+            panel6 = new Panel();
+            txtFilter = new TextBox();
+            dataGridForEvents = new DataGridView();
+            panel5.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridForEvents).BeginInit();
+            SuspendLayout();
+            // 
+            // panel5
+            // 
+            panel5.Controls.Add(panel6);
+            panel5.Controls.Add(txtFilter);
+            panel5.Location = new Point(351, 15);
+            panel5.Name = "panel5";
+            panel5.Size = new Size(499, 51);
+            panel5.TabIndex = 6;
+            // 
+            // panel6
+            // 
+            panel6.BackColor = Color.White;
+            panel6.ForeColor = Color.FromArgb(30, 30, 30);
+            panel6.Location = new Point(3, 36);
+            panel6.Name = "panel6";
+            panel6.Size = new Size(477, 1);
+            panel6.TabIndex = 4;
+            // 
+            // txtFilter
+            // 
+            txtFilter.Anchor = AnchorStyles.None;
+            txtFilter.BackColor = Color.FromArgb(30, 30, 30);
+            txtFilter.BorderStyle = BorderStyle.None;
+            txtFilter.Font = new Font("Segoe UI", 13F);
+            txtFilter.ForeColor = Color.Gray;
+            txtFilter.Location = new Point(3, 5);
+            txtFilter.Name = "txtFilter";
+            txtFilter.PlaceholderText = "Search A Ticket";
+            txtFilter.Size = new Size(477, 29);
+            txtFilter.TabIndex = 2;
+            txtFilter.TextChanged += txtFilter_TextChanged;
+            // 
+            // dataGridForEvents
+            // 
+            dataGridForEvents.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridForEvents.Dock = DockStyle.Bottom;
+            dataGridForEvents.Location = new Point(0, 102);
+            dataGridForEvents.Name = "dataGridForEvents";
+            dataGridForEvents.RowHeadersWidth = 51;
+            dataGridForEvents.Size = new Size(1134, 613);
+            dataGridForEvents.TabIndex = 5;
+            // 
+            // BoughtTicketsForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(30, 30, 30);
+            ClientSize = new Size(1134, 715);
+            Controls.Add(panel5);
+            Controls.Add(dataGridForEvents);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "BoughtTicketsForm";
+            Text = "BoughtTicketsForm";
+            panel5.ResumeLayout(false);
+            panel5.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridForEvents).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel5;
+        private Panel panel6;
+        private TextBox txtFilter;
+        private DataGridView dataGridForEvents;
+    }
+}
---
+++ EventTicketForms/BoughtTicketsForm.cs
@@ -0,0 +1,54 @@
+﻿using EventTicketForms.Resources;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EventTicketForms
+{
+    public partial class BoughtTicketsForm : Form
+    {
+        private readonly string myTickets = "http://localhost:5172/api/Ticket/viewmytickets";
+        private string _token;
+        public BoughtTicketsForm()
+        {
+            InitializeComponent();
+            _token = TokenManager.Token;
+            SeeMyTickets();
+        }
+        private async void SeeMyTickets()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", _token);
+                using (HttpResponseMessage response = await client.GetAsync(myTickets))
+                {
+                    if (Convert.ToInt32(response.StatusCode) == 401)
+                    {
+                        MessageBox.Show("Unauthorized");
+                    }
+                    else if (Convert.ToInt32(response.StatusCode) == 200)
+                    {
+                        using (HttpContent content = response.Content)
+                        {
+                            var data = content.ReadAsStringAsync();
+
+
+                        }
+                    }
+                    else { MessageBox.Show("Error"); }
+                }
+            }
+        }
+        private void txtFilter_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
